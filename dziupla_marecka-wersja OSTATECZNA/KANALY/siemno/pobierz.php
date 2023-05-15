bc998a62586ab16f212188bc7cce68ba29751f73
Artur Zakrzewski <90864643+MagiczneMaselko@users.noreply.github.com>
2023-05-15 21:46:45 +0200

    Delete dziupla_marecka-wersja OSTATECZNA/KANALY/siemno directory

--- dziupla_marecka-wersja OSTATECZNA/KANALY/siemno/pobierz.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if($_GET["plik"] != "") {
-		$file_url = "./PLIKI/".$_GET["plik"];
-		header('Content-Type: application/octet-stream');
-		header("Content-Transfer-Encoding: Binary"); 
-		header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\""); 
-		readfile($file_url); 
-	}
-?>
